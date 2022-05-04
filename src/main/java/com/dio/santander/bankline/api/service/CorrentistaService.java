78e7c6319d5be1cbeb63d24e66fbd083059eee8f
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-04 01:59:44 -0400

    criados os servicos com as regras de negocio de movimentacao e correntista

---
+++ src/main/java/com/dio/santander/bankline/api/service/CorrentistaService.java
@@ -0,0 +1,33 @@
+package com.dio.santander.bankline.api.service;
+
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
+import com.dio.santander.bankline.api.model.Conta;
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class CorrentistaService {
+
+    private final CorrentistaRepository correntistaRepository;
+
+    public CorrentistaService(CorrentistaRepository correntistaRepository) {
+        this.correntistaRepository = correntistaRepository;
+    }
+
+    public void save(NovoCorrentista novoCorrentista) {
+        Correntista correntista = new Correntista();
+        correntista.setCpf(novoCorrentista.getCpf());
+        correntista.setNome(novoCorrentista.getNome());
+
+        Conta conta = new Conta();
+        //inicia a conta com saldo zerado.
+        conta.setSaldo(0.0);
+        //regra arbitraria que define o numero da conta baseado na data atual
+        conta.setNumero(new Date().getTime());
+        correntista.setConta(conta);
+        correntistaRepository.save(correntista);
+    }
+}
---
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -0,0 +1,49 @@
+package com.dio.santander.bankline.api.service;
+
+import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.model.Movimentacao;
+import com.dio.santander.bankline.api.model.MovimentacaoTipo;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class MovimentacaoService {
+
+    private final MovimentacaoRepository movimentacaoRepository;
+    private final CorrentistaRepository correntistaRepository;
+
+    public MovimentacaoService(MovimentacaoRepository movimentacaoRepository, CorrentistaRepository correntistaRepository) {
+        this.movimentacaoRepository = movimentacaoRepository;
+        this.correntistaRepository = correntistaRepository;
+    }
+
+    public void save(NovaMovimentacao novaMovimentacao) {
+
+        Movimentacao movimentacao = new Movimentacao();
+
+        Double valor = novaMovimentacao.getTipo() ==
+                MovimentacaoTipo.RECEITA ? novaMovimentacao.getValor() : novaMovimentacao.getValor() * -1;
+        movimentacao.setValor(valor);
+        movimentacao.setDataHora(LocalDateTime.now());
+        movimentacao.setDescricao(novaMovimentacao.getDescricao());
+        movimentacao.setIdConta(novaMovimentacao.getIdConta());
+        movimentacao.setTipo(novaMovimentacao.getTipo());
+
+        Correntista correntista = correntistaRepository.findById(novaMovimentacao.getIdConta()).orElse(null);
+        if(correntista != null) {
+            correntista.getConta().setSaldo(correntista.getConta().getSaldo() + valor);
+            correntistaRepository.save(correntista);
+        }
+
+        movimentacaoRepository.save(movimentacao);
+    }
+
+    public List<Movimentacao> findAll() {
+        return movimentacaoRepository.findAll();
+    }
+}
