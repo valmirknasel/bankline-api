6a6b72c31aa0fc15e8108fe4ffa31a9b635a076b
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2022-05-17 13:31:24 -0400

    ajustes na nomenclatura dos atributos da classe para refletir a as regras de negocio

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,6 +1,9 @@
 package com.dio.santander.bankline.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -13,17 +16,24 @@ public class Movimentacao implements Serializable {
     private Integer id;
 
     @Column(name = "data_hora")
+    @NotNull
     private LocalDateTime dataHora;
 
+    @NotNull
     private String descricao;
     private Double valor;
 
-    @Column(name = "id_correntista")
-    private Integer idCorrentista;
-
     //@Enumerated grava o valor literal do enum no banco
     @Enumerated(EnumType.STRING)
-    private MovimentacaoTipo tipo;
+    @NotNull
+    private MovimentacaoTipo tipoMovimentacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_correntista")
+    @NotNull
+    @JsonIgnore()
+    private Correntista correntista;
+
 
     public Integer getId() {
         return id;
@@ -57,19 +67,19 @@ public class Movimentacao implements Serializable {
         this.valor = valor;
     }
 
-    public Integer getIdCorrentista() {
-        return idCorrentista;
+    public @NotNull Correntista getCorrentista() {
+        return correntista;
     }
 
-    public void setIdCorrentista(Integer idConta) {
-        this.idCorrentista = idConta;
+    public void setCorrentista(Correntista correntista) {
+        this.correntista = correntista;
     }
 
-    public MovimentacaoTipo getTipo() {
-        return tipo;
+    public MovimentacaoTipo getTipoMovimentacao() {
+        return tipoMovimentacao;
     }
 
-    public void setTipo(MovimentacaoTipo tipo) {
-        this.tipo = tipo;
+    public void setTipoMovimentacao(MovimentacaoTipo tipo) {
+        this.tipoMovimentacao = tipo;
     }
 }
